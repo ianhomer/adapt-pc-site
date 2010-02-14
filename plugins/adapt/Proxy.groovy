8e875d8800fd229f7c04b43b9365c32964b9bd91
Ian Homer <ian.homer@bemoko.com>
2010-02-14 18:20:23 +0000

    Support for adapt pc site plugin

---
+++ plugins/adapt/Proxy.groovy
@@ -0,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright 2010 bemoko 
+ */
+package adapt 
+
+import com.bemoko.live.platform.mwc.plugins.AbstractPlugin
+
+/**
+ * Plugin that proxies to the underlying application site.
+ */
+ 
+/*
+ * We use HtmlUnit to process HTML page so we need to load these dependencies
+ */
+@Grab(group='net.sourceforge.htmlunit', module='htmlunit', version='2.6')
+@Grab(group='net.sourceforge.htmlunit', module='htmlunit-core-js', version='2.6')
+@Grab(group='commons-httpclient', module='commons-httpclient', version='3.1')
+@Grab(group='net.sourceforge.cssparser', module='cssparser', version='0.9.5')
+
+class Proxy extends AbstractPlugin {
+  def contentId
+  def url
+  def content
+      
+  void initialise(Map p) {
+    contentId = p.contentId
+    if (!contentId) {
+      throw new Exception("Content ID must be set when you initialise this plugin")
+    }
+    url = p.url
+  }
+  
+  void execute(Map p) {
+    
+    /*
+     * Create a web client which we will use to proces the page.  We'll disable CSS and
+     * JavaScript to optimise the process flow
+     */
+    def webClient = new com.gargoylesoftware.htmlunit.WebClient()
+    webClient.cssEnabled=false
+    webClient.javaScriptEnabled=false
+    webClient.throwExceptionOnScriptError=false
+    def page
+    try {
+      page = getPage(p,webClient)
+      
+      /*
+       * If we've got a page object then we can continue with the processing of it
+       */
+      if (page) {
+        content = page.getElementById(contentId)?.asXml()
+      } else {
+        log.error("Model not loaded")
+      }
+    } finally {
+      /*
+       * Free up resources from web client
+       */
+      webClient.closeAllWindows()
+    }
+  }
+  
+  def getPage(p,webClient) {
+    return webClient.getPage(url)
+  }
+  
+}
---
+++ plugins/adapt/ProxyAsWebService.groovy
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * Copyright 2010 bemoko 
+ */
+package adapt 
+
+class ProxyAsWebService extends Proxy {
+  private static DEFAULT_ORIGINATING_URL=new URL("http://bemoko.com/")
+  private static pageCreator=new com.gargoylesoftware.htmlunit.DefaultPageCreator()
+
+  /*
+   * If non-mobile status code is non-zero, then if a non-mobile device connects
+   * then the given status code will be returned without any page processing.  This
+   * provides a convenient mechanism for a web service client to identify that
+   * content need not be transformed and can be delivered as is
+   */
+  int nonMobilStatusCode = 0
+  
+  void initialise(Map p) {
+    if (p.nonMobilStatusCode) {
+      nonMobilStatusCode = p.nonMobilStatusCode
+    }
+    super.initialise(p)
+  }
+
+  void execute(Map p) {
+    if (nonMobilStatusCode > 0 && user.device.isMobile == 'false') {
+      /*
+       * For non-mobile devices we return a 204 so the calling process knows that it can 
+       * deal with the response as a PC response
+       */
+      log.info("Not mobile")        
+      platform.response.status = 204
+      return
+    }
+
+    super.execute(p)
+  }
+
+  def getPage(p,webClient) {
+    if (!p.markup) {
+      throw new Exception("No markup parameter specified")
+    }
+    log.info("Markup = ${p.markup}")
+    /*
+     * Generate the page from HTML sent in as intent parameter, 
+     * e.g. form post or api call
+     */
+    def window=new com.gargoylesoftware.htmlunit.TopLevelWindow("top",webClient)  
+    def webResponse=new
+      com.gargoylesoftware.htmlunit.StringWebResponse(p.markup,DEFAULT_ORIGINATING_URL) 
+    return pageCreator.createPage(webResponse,window)
+  }
+}
